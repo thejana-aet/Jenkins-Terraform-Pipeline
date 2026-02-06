65193f8d2aa5898ed3908af580b8a5226cf52ea7
thejana-aet <thejanagalagama@gmail.com>
2026-02-06 13:04:33 +0530

    bug fixes

--- main.tf
+++ main.tf
@@ -1,84 +1,84 @@
-resource "aws_vpc" "main" {
-  cidr_block       = "10.3.0.0/16"
-  instance_tenancy = "default"
+# resource "aws_vpc" "main" {
+#   cidr_block       = "10.3.0.0/16"
+#   instance_tenancy = "default"
 
-  tags = {
-    Name = "thejana-vpc-secondary"
-  }
-}
+#   tags = {
+#     Name = "thejana-vpc-secondary"
+#   }
+# }
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.3.1.0/24"
+# resource "aws_subnet" "public_subnet" {
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = "10.3.1.0/24"
 
-  tags = {
-    Name = "thejana-public-subnet-secondary"
-  }
-}
+#   tags = {
+#     Name = "thejana-public-subnet-secondary"
+#   }
+# }
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.3.2.0/24"
+# resource "aws_subnet" "private_subnet" {
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = "10.3.2.0/24"
 
-  tags = {
-    Name = "thejana-private-subnet-secondary"
-  }
-}
+#   tags = {
+#     Name = "thejana-private-subnet-secondary"
+#   }
+# }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+# resource "aws_internet_gateway" "igw" {
+#   vpc_id = aws_vpc.main.id
 
-  tags = {
-    Name = "thejana-igw-secondary"
-  }
-}
+#   tags = {
+#     Name = "thejana-igw-secondary"
+#   }
+# }
 
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main.id
+# resource "aws_route_table" "public_rt" {
+#   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-  tags = {
-    Name = "thejana-public-rt-secondary"
-  }
-}
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.igw.id
+#   }
+#   tags = {
+#     Name = "thejana-public-rt-secondary"
+#   }
+# }
 
-resource "aws_route_table_association" "public_rt_assoc" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
-}
+# resource "aws_route_table_association" "public_rt_assoc" {
+#   subnet_id      = aws_subnet.public_subnet.id
+#   route_table_id = aws_route_table.public_rt.id
+# }
 
-resource "aws_security_group" "public_sg" {
-  name        = "thejana-public-sg"
-  description = "Allow SSH traffic"
-  vpc_id      = aws_vpc.main.id
+# resource "aws_security_group" "public_sg" {
+#   name        = "thejana-public-sg"
+#   description = "Allow SSH traffic"
+#   vpc_id      = aws_vpc.main.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = "thejana-public-sg-secondary"
-  }
-}
+#   tags = {
+#     Name = "thejana-public-sg-secondary"
+#   }
+# }
 
 resource "aws_instance" "ec2_instance1" {
   ami                         = "ami-0b6c6ebed2801a5cb"
   instance_type               = "t3.micro"
-  subnet_id                   = aws_subnet.public_subnet.id
-  vpc_security_group_ids      = [aws_security_group.public_sg.id]
+#   subnet_id                   = aws_subnet.public_subnet.id
+#   vpc_security_group_ids      = [aws_security_group.public_sg.id]
   associate_public_ip_address = true
   key_name                    = "aws_key_test"
 
